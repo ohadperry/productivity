29177a6f7f195ee342387b91535baad4b31ef131
Ohad <ohad.perry9@gmail.com>
2018-01-01 12:21:58 +0200

    docker command

--- bash_methods_dockers.sh
+++ bash_methods_dockers.sh
@@ -1,7 +1,5 @@
 
-function show_diff {
-    DEFAULT_MAIN_BRANCH=master
-    MAIN_BRANCH=${2:-$DEFAULT_MAIN_BRANCH}
-    echo "comparing between $1 and $2"
-    git log --left-right --graph --cherry-pick --oneline $1...$MAIN_BRANCH
+function enter_to_docker_container(){
+    CONTAINER_NAME=$1
+    docker exec -it $CONTAINER_NAME /bin/bash
 }
