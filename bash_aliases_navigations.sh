782940100b1cb401507449d3650d6b500fec756f
Ohad <ohad.perry9@gmail.com>
2019-08-25 11:12:36 +0300

    aliases

--- bash_aliases_navigations.sh
+++ bash_aliases_navigations.sh
@@ -15,3 +15,5 @@ alias oh="cd ~/.oh-my-zsh"
 alias bl="dh; how-build-your-own-blockchain"
 # alias serverless_demo="dh; cd myBackendAPI"
 # alias bd="serverless_demo"
+# python software temp files saves here 
+alias temp_folder="cd /var/folders/m3/xqv5btyj1gg5zk4vsbj8_5p40000gq/T"
